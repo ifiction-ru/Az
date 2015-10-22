7084f3f3df91d6f336f865b70c4d3b897a1b7b22
Oleg Aleynikov <oleg@aleynikov.com>
2015-10-22 20:35:08 +0300

    Переработан вызов события, доработки в части локализации
    Переработан вызов события — теперь нормально формируется фильтр поиска и
    набор данных для передачи в функцию-обработчик.
    Доработки в части локализации.

--- precursor/az/az-events.js
+++ precursor/az/az-events.js
@@ -14,6 +14,7 @@ window.EVENTS = (function() {
     //----------
     var fields_localization = {
         'event':    'event',
+        'location': 'location',
         'when':     'when',
         'what':     'what',
         'who':      'what',
@@ -22,8 +23,19 @@ window.EVENTS = (function() {
         'to':       'to',
     };
     //----------
+    var fields_localization_cache = {
+        'event':    ['event'],
+        'location': ['location'],
+        'when':     ['when'],
+        'what':     ['what', 'who'],
+        'where':    ['where'],
+        'from':     ['from'],
+        'to':       ['to'],
+    };
+    //----------
     var fields_settings = {
         'event':    {save:'asis'},
+        'location': {save:'object'},
         'when':     {save:'asis'},
         'what':     {save:'object'},
         'where':    {save:'object'},
@@ -32,20 +44,21 @@ window.EVENTS = (function() {
     };
     //----------
     var events_sort = {
-        'put':    'what logicaldesc, to logicaldesc',
-        'remove': 'what logicaldesc, from logicaldesc',
-        'move':   'what logicaldesc, from logicaldesc, to logicaldesc',
+        'put':    'location logicaldesc, what logicaldesc, to logicaldesc',
+        'remove': 'location logicaldesc, what logicaldesc, from logicaldesc',
+        'move':   'location logicaldesc, what logicaldesc, from logicaldesc, to logicaldesc',
     };
     //--------------------------------------------------
     function _get_template (_event) {
+        var loc = AZ.getLocation(true);
         if (_event == 'put') {
-            return {'event':'put', 'when':BEFORE, 'what':null, 'to':null};
+            return {'event':'put', 'location':loc, 'when':BEFORE, 'what':null, 'to':null};
             
         } else if (_event == 'remove') {
-            return {'event':'remove', 'when':BEFORE, 'what':null, 'from':null};
+            return {'event':'remove', 'location':loc, 'when':BEFORE, 'what':null, 'from':null};
             
         } else if (_event == 'move') {
-            return {'event':'move', 'when':BEFORE, 'what':null, 'from':null, 'to':null};
+            return {'event':'move', 'location':loc, 'when':BEFORE, 'what':null, 'from':null, 'to':null};
             
         } else {
             return undefined;
@@ -58,6 +71,10 @@ window.EVENTS = (function() {
             //----------
             for (var k in _locale) {
                 fields_localization[k] = _locale[k];
+                //----------
+                var arr = fields_localization_cache[_locale[k]]
+                //----------
+                if (arr.indexOf(k) == -1) {arr.push(k);} // end if
             } // end for k
             //----------
         },
@@ -131,17 +148,40 @@ window.EVENTS = (function() {
             // var db = TAFFY(); db.insert({a:'ИГРОК'}); db.insert({a:null}); db.insert({a:'ОСТРОВ'}); db().order('a logicaldesc').select('a');
             // var db = TAFFY(); db.insert({a:'ИГРОК', b:null}); db.insert({a:'ИГРОК', b:'Ok'}); db.insert({a:null}); db.insert({a:'ОСТРОВ', b:'Hello'}); db().order('a logicaldesc, b logicaldesc').select('a','b');
             //----------
-            var reactions = db(_event).order(events_sort[event_name]).get();
+            var search  = {};
+            var data    = {};
+            //----------
+            for (var fname in _event) {
+                if (fields_settings[fname].save == 'object') {
+                    search[fname]   = [_event[fname], null];
+                    data[fname]     = AZ.getObject(_event[fname]);
+                } else {
+                    search[fname]   = _event[fname];
+                    data[fname]     = _event[fname];
+                } // end if
+                //----------
+                var arr = fields_localization_cache[fname];
+                for (var x=0; x<arr.length; x++) {
+                    var fname2 = arr[x];
+                    if (fname2 != fname) {
+                        data[fname2] = data[fname];
+                    } // end if
+                } // end for
+            } // end for fname
+            //----------
+            var reactions = db(search).order(events_sort[event_name]).get();
             //----------
             if (reactions == false) {return true;} // end if
             //----------
             var result = true;
             //----------
+            var converted = false;
+            //----------
             for (var x=0; x<reactions.length; x++) {
                 var rec = reactions[x];
                 //----------
                 if (typeof(rec.module) == 'function') {
-                    var res = rec.module(_event);
+                    var res = rec.module(data);
                     //----------
                     if (res === false) {result = false;} // end if
                 } // end if
