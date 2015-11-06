763e5b0ebb2eaf02a811b4874953cf464c642808
Oleg Aleynikov <oleg@aleynikov.com>
2015-11-06 17:25:24 +0300

    Добавлен прототип объекта "Текст"
    Добавлен прототип объекта "Текст" (az-texts).

---
+++ precursor/az/az-texts.js
@@ -0,0 +1,62 @@
+/* --------------------------------------------------------------------------- */
+// РАБОТА С ТЕКСТАМИ
+//--------------------------------------------------
+window.SimpleText = function (_id, _text, _module) {
+    tSimpleObject.apply(this, arguments);
+    //----------
+    this.type           = 'text'; // Тип объекта
+    this.module         = _module;
+    //----------
+    this.pages = {texts:[], current:-1};
+    // Нарезаем текст на страницы
+    this.pages.texts = _text.split(/[\s|\n]*\<\-\s*далее\s*\-\>[\s|\n]*/gi);
+    for (var x=0; x<this.pages.texts.length; x++) {
+        this.pages.texts[x] = this.pages.texts[x].trim().replace(/^\s+/gm, '\n');
+    } // end for
+    //----------
+    //Object.defineProperty(this, 'isObject', {configurable:false, writable:false, value:true});
+    //Object.defineProperty(this, 'ID',       {configurable:false, writable:false, value:objectID});
+    //Object.defineProperty(this, 'pages',    {configurable:false, writable:false, value:pages});
+    //Object.defineProperty(this, 'module',   {configurable:false, writable:false, value:module});
+    //----------
+} // end function "SimpleText"
+SimpleText.prototype = Object.create(tSimpleObject.prototype);
+SimpleText.prototype.constructor = SimpleText;
+//--------------------------------------------------
+SimpleText.prototype.next = function () {
+    //----------
+    if (this.pages.current < this.pages.texts.length - 1) {
+        this.pages.current++;
+        //----------
+        print(this.pages.texts[this.pages.current]);
+    } else {
+        this.pages.current = -1;
+        //----------
+        AZ.outputLayers.pop();
+        //----------
+        if (typeof(this.module) == 'function') {
+            this.module();
+        } // end if
+        //----------
+        AZ.updateAvailableObjects();
+        //----------
+        PARSER.pre_parse();
+        DEBUG.updateWordsFullList();
+        DEBUG.updateWordsShortList();
+    } // end if
+    //----------
+} // end function "<SimpleText>.Next"
+//--------------------------------------------------
+SimpleText.prototype.print = function () {
+    //----------
+    AZ.outputLayers.push(this);
+    //----------
+    AZ.updateAvailableObjects();
+    //this.pages.current = -1;
+    //----------
+    this.next();
+    //----------
+} // end function "<SimpleText>.Next"
+//--------------------------------------------------
+//SimpleText.prototype.Action = tSimpleObject.prototype.Action;
+/* --------------------------------------------------------------------------- */
