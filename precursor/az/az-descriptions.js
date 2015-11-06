b21696f4221bad0cc166fda439cd02d34d4af70c
Oleg Aleynikov <oleg@aleynikov.com>
2015-11-06 17:27:02 +0300

    Работа со счётчиками описаний
    Работа со счётчиками описаний.

--- precursor/az/az-descriptions.js
+++ precursor/az/az-descriptions.js
@@ -58,7 +58,18 @@ tDescription.prototype.getTitle = function (_param, _html) {
 }; // end function "<Объект>.getTitle"
 /* --------------------------------------------------------------------------- */
 tDescription.prototype.getText = function (_param, _html) {
+    /*var counter = getProperty(this.OWNER, 'descr.counter');
+    if (counter === undefined) {
+        setProperty(this.OWNER, 'descr.counter', 1);
+        counter = 1;
+    } // end if
+    //----------
+    if (_param === undefined) {
+        _param = {'счётчик': counter};
+    } // end if*/
     var text    = (typeof(this.descr_text) == 'function') ? this.descr_text(_param) : this.descr_text;
+    //----------
+    //setProperty(this.OWNER, 'descr.counter', counter+1);
     //----------
     var ownerID = AZ.getID(this.OWNER);
     //----------
